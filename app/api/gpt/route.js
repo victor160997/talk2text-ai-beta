44d53025f343a67bc04a7f8b3749fa553eaf5c57
Victor Rodrigues <victor.men.dev@gmail.com>
2023-09-12 17:00:18 -0300

    colocando env no codigo

--- app/api/gpt/route.js
+++ app/api/gpt/route.js
@@ -1,13 +1,10 @@
-import * as dotenv from "dotenv";
 import { OpenAI } from "langchain/llms/openai";
-
-dotenv.config();
 
 export const POST = async (request) => {
   try {
     const { text } = await request.json();
     const llm = new OpenAI({
-      openAIApiKey: process.env.OPENAI_API_KEY,
+      openAIApiKey: "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",
       temperature: 1,
       modelName: "gpt-3.5-turbo-16k"
     });
