fd427c9faf8b632df733887b8e7e790aeed88c76
Victor Rodrigues <victor.men.dev@gmail.com>
2023-09-12 17:10:39 -0300

    mudando variável

--- app/api/gpt/route.js
+++ app/api/gpt/route.js
@@ -4,11 +4,11 @@ export const POST = async (request) => {
   try {
     const { text } = await request.json();
     const llm = new OpenAI({
-      openAIApiKey: process.env.OPENAI_API_KEY,
+      openAIApiKey: process.env.ZAPIER_NLA_API_KEY,
       temperature: 1,
       modelName: "gpt-3.5-turbo-16k"
     });
-    const result = await llm.predict(`Transforme essa transcrição de audio em um texto deixando o mais fiel possível ao audio: ${text}`);
+    const result = await llm.predict(`Transforme essa transcrição de audio em texto deixando o mais fiel possível ao audio: ${text}`);
 
     return new Response(JSON.stringify(result), { status: 200 })
   } catch (error) {
