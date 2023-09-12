f194a87b08f3611d096cdcd094ca11b7c47e05cb
Victor Rodrigues <victor.men.dev@gmail.com>
2023-09-12 16:36:24 -0300

    integrando com gpt

--- app/api/gpt/route.js
+++ app/api/gpt/route.js
@@ -1,9 +1,20 @@
-export const GET = async (request) => {
-    try {
-        return new Response(JSON.stringify({
-            message: "Olá mundo"
-        }), { status: 200 })
-    } catch (error) {
-        return new Response("Failed to fetch data", { status: 500 })
-    }
+import * as dotenv from "dotenv";
+import { OpenAI } from "langchain/llms/openai";
+
+dotenv.config();
+
+export const POST = async (request) => {
+  try {
+    const { text } = await request.json();
+    const llm = new OpenAI({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      temperature: 1,
+      modelName: "gpt-3.5-turbo-16k"
+    });
+    const result = await llm.predict(`Transforme essa transcrição de audio em um texto deixando o mais fiel possível ao audio: ${text}`);
+
+    return new Response(JSON.stringify(result), { status: 200 })
+  } catch (error) {
+      return new Response("Failed to fetch data", { status: 500 })
+  }
 } 
